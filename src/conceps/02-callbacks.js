8b656d38db4de91d170d4095444d0f553424e51d
LillaDou <lilla.e.j.du@gmail.com>
2024-07-01 18:12:23 +0200

    Manejo de errores en los callbacks

--- src/conceps/02-callbacks.js
+++ src/conceps/02-callbacks.js
@@ -8,8 +8,19 @@ import { heroes } from "../data/heroes";
 export const callbacksComponent = ( element ) => {
 
     const id = '5d86371fd55e2e2a30fe1ccb1';
-    findHero( id, (hero) => {
+    findHero( id, ( error, hero) => {
+
+        //Si el error existe...
+        if ( error ) {
+            element.innerHTML = error; 
+            return;
+        }
+
         element.innerHTML = hero.name;
+
+        // element.innerHTML = hero?.name || 'No hay héroe';
+        //'?': Si el heroe existe/tiene valor, busca la propiedad .name.
+        //Si no tiene valor, será undefined
     } );
     
 }
@@ -17,12 +28,21 @@ export const callbacksComponent = ( element ) => {
 /**
  * 
  * @param {String} id 
- * @param { (hero: Object)=> void } callback Argumento: hero. Es una función que no regresa nada
+ * @param { (error?: String, hero: Object)=> void } callback Argumento: error (si hay un error: undefined), 
+ * hero (objecto). Es una función que no regresa nada
  */
 const findHero = ( id, callback ) => {
 
     const hero = heroes.find( hero => hero.id === id );
     //.find para buscar a un heroe
 
-    callback( hero );
+    //Validación para ver si hay algún error.
+    if ( !hero ) {
+        callback(`Hero with id ${ id } not found.`);
+        return; // undefined;
+    }
+
+    callback( null, hero );
+    //Ponemos null porque debemos dar dos argumentos a la función: un 'error'
+    // y luego el hero (ver documentación de findHero)
 }
