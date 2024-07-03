e76f667deb88e8c2823c1e3ae41dbeec644887d8
LillaDou <lilla.e.j.du@gmail.com>
2024-07-03 15:21:25 +0200

    Utilizar una promesa

--- src/conceps/03-promises.js.js
+++ src/conceps/03-promises.js.js
@@ -7,7 +7,26 @@ import { heroes } from "../data/heroes"
  */
 export const promiseComponent = ( element ) => {
 
+  const renderHero = ( hero ) => {
+    element.innerHTML = hero.name;
+  }
 
+  const renderError = (error) => {
+    element.innerHTML = `
+      <h1>Error:</h1>
+      <h3>${ error }</h3>`;
+  }
+
+  const id1 = '5d86371f25a058e5b1c8a65e';
+
+  findHero(id1)
+    .then( renderHero )
+    // .then( superHero => renderHero( superHero) );
+    // Cuando el argumento (superHero) también es lo que mandamos al argumento
+    //de la función de renderHero, entonces se puede acortar y poner solo
+    // el nombre de la función.
+    // .catch( error => renderError( error ) );
+    .catch( renderError );
   
 }
 
