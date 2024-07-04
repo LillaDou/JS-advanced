9b0e9cb32b64bed91bdf2e87ceb34481adc4bdbf
LillaDou <lilla.e.j.du@gmail.com>
2024-07-04 11:12:11 +0200

    Promise All y otra forma más límpia y clara de hacer el promise hell

--- src/conceps/03-promises.js.js
+++ src/conceps/03-promises.js.js
@@ -25,19 +25,45 @@ export const promiseComponent = ( element ) => {
   }
 
   const id1 = '5d86371f25a058e5b1c8a65e';
-  const id2 = '5d86371f233c9f2425f16916';
-  
-  //Promise Hell
-  findHero( id1 )
-    .then( (hero1) => {
+  const id2 = '5d86371f97c29d020f1e1f6d';
 
-      findHero( id2 )
-        .then( (hero2) =>{
-          renderTwoHeroes(hero1, hero2)
-        })
-        .catch( renderError);
-    })
-    .catch( renderError );
+  //! Promise.all([])
+  //Mandamos a ejecutar +1 promesas de manera simultánea, siempre que las promesas
+  //no dependan del restultado entre sí. Son promesas independientes
+  Promise.all([
+    findHero(id1),
+    findHero(id2),
+  ])
+  .then( ([hero1, hero2]) => renderTwoHeroes( hero1, hero2 ) )
+  .catch( renderError ); //Si una promesa del findHero da error, todo da error
+
+
+  //! Promise Hell - Forma 2:
+  // Esta forma sirve para ejecutar varias promesas cuyos resultados dependen entre si. 
+
+  // let hero1;
+
+  // findHero( id1 )
+  //   .then( hero => {
+  //     hero1 = hero;
+  //     return findHero( id2 );
+  //   }).then( hero2 => {
+  //     renderTwoHeroes( hero1, hero2 );
+  //   })
+  //   .catch( renderError );
+
+
+  //!Promise Hell - Forma 1:
+  // findHero( id1 )
+  //   .then( (hero1) => {
+
+  //     findHero( id2 )
+  //       .then( (hero2) =>{
+  //         renderTwoHeroes(hero1, hero2)
+  //       })
+  //       .catch( renderError);
+  //   })
+  //   .catch( renderError );
 
   //? findHero(id1)
   //?   .then( renderHero )
