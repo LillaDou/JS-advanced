e353261bda1e5059a92560d2fbfd5cc2e2a1570f
LillaDou <lilla.e.j.du@gmail.com>
2024-07-04 10:55:10 +0200

    Promise Hell

--- src/conceps/03-promises.js.js
+++ src/conceps/03-promises.js.js
@@ -11,6 +11,13 @@ export const promiseComponent = ( element ) => {
     element.innerHTML = hero.name;
   }
 
+  const renderTwoHeroes = (hero1, hero2) => {
+    element.innerHTML = `
+      <h3>${hero1.name}</h3>
+      <h3>${hero2.name}</h3>
+    `;
+  }
+
   const renderError = (error) => {
     element.innerHTML = `
       <h1>Error:</h1>
@@ -18,15 +25,28 @@ export const promiseComponent = ( element ) => {
   }
 
   const id1 = '5d86371f25a058e5b1c8a65e';
+  const id2 = '5d86371f233c9f2425f16916';
+  
+  //Promise Hell
+  findHero( id1 )
+    .then( (hero1) => {
 
-  findHero(id1)
-    .then( renderHero )
+      findHero( id2 )
+        .then( (hero2) =>{
+          renderTwoHeroes(hero1, hero2)
+        })
+        .catch( renderError);
+    })
+    .catch( renderError );
+
+  //? findHero(id1)
+  //?   .then( renderHero )
     // .then( superHero => renderHero( superHero) );
     // Cuando el argumento (superHero) también es lo que mandamos al argumento
     //de la función de renderHero, entonces se puede acortar y poner solo
     // el nombre de la función.
     // .catch( error => renderError( error ) );
-    .catch( renderError );
+    //? .catch( renderError );
   
 }
 
