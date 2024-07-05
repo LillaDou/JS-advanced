e89ea9b3b2d81ff5e3d6eaaef39818d0a5834e56
LillaDou <lilla.e.j.du@gmail.com>
2024-07-05 20:17:06 +0200

    Errores en Async - await

--- src/conceps/06-async-await.js
+++ src/conceps/06-async-await.js
@@ -9,14 +9,22 @@ export const asyncAwaitComponent = async( element ) => {
 
   const id1 = '5d86371f2343e37870b91ef1';
   const id2 = '5d86371f25a058e5b1c8a65e';
+
+  try {
+    const hero1 =  await findHero( id1 );
+    const hero2 =  await findHero( id2 );
+    //El await es como decir: espera a que el resultado de la función findHero termine, 
+    //y cuando termine, el producto de esa promesa será el valor de hero1
   
-  const hero1 =  await findHero( id1 );
-  const hero2 =  await findHero( id2 );
-  //El await es como decir: espera a que el resultado de la función findHero termine, 
-  //y cuando termine, el producto de esa promesa será el valor de hero1
-
-
-  element.innerHTML = `${ hero1.name } / ${ hero2.name }`;
+    element.innerHTML = `${ hero1.name } / ${ hero2.name }`;
+  
+  } catch (error) {
+      element.innerHTML = error;
+  }
+  //Con el try y catch indicamos si algo sale mal. En este caso, hace la prueba
+  //de los nombres de los héroes. En caso de que no funcione, muestra en pantalla 
+  //cuál ha sido el error. 
+  
 }
 
 
